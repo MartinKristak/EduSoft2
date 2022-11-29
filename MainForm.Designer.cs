6037a481ffdf6c9f24533d56bf7f58996839b476
MartinKristak <kristak.martin44@gmail.com>
2022-11-29 21:30:50 +0100

    premenovane tlacidla

--- MainForm.Designer.cs
+++ MainForm.Designer.cs
@@ -353,7 +353,7 @@ namespace Edusoft2
 			this.button5.Name = "button5";
 			this.button5.Size = new System.Drawing.Size(104, 80);
 			this.button5.TabIndex = 9;
-			this.button5.Text = "Načítať v hre";
+			this.button5.Text = "Nová sada";
 			this.button5.UseVisualStyleBackColor = true;
 			// 
 			// button4
@@ -362,7 +362,7 @@ namespace Edusoft2
 			this.button4.Name = "button4";
 			this.button4.Size = new System.Drawing.Size(104, 80);
 			this.button4.TabIndex = 8;
-			this.button4.Text = "Uložiť";
+			this.button4.Text = "Uložiť mapu";
 			this.button4.UseVisualStyleBackColor = true;
 			this.button4.Visible = false;
 			// 
@@ -372,7 +372,7 @@ namespace Edusoft2
 			this.button3.Name = "button3";
 			this.button3.Size = new System.Drawing.Size(104, 80);
 			this.button3.TabIndex = 7;
-			this.button3.Text = "Načítať";
+			this.button3.Text = "Načítať mapu";
 			this.button3.UseVisualStyleBackColor = true;
 			this.button3.Visible = false;
 			// 
@@ -382,7 +382,7 @@ namespace Edusoft2
 			this.button2.Name = "button2";
 			this.button2.Size = new System.Drawing.Size(104, 80);
 			this.button2.TabIndex = 6;
-			this.button2.Text = "Testovací";
+			this.button2.Text = "Vytváranie mapy";
 			this.button2.UseVisualStyleBackColor = true;
 			this.button2.Click += new System.EventHandler(this.Button2Click);
 			this.button3.Click += new System.EventHandler(this.Button3Click);
@@ -396,7 +396,7 @@ namespace Edusoft2
 			this.button1.Name = "button1";
 			this.button1.Size = new System.Drawing.Size(104, 80);
 			this.button1.TabIndex = 5;
-			this.button1.Text = "Hrací";
+			this.button1.Text = "Hrací režim";
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.Button1Click);
 			// 
